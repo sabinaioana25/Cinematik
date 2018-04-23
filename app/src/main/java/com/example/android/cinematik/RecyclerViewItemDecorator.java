4dad37056e5cef94ac1fcaaff446006f4a792d70
Sabina Ioana <sabinaleitoiu91@gmail.com>
2018-04-23 19:08:12 +0100

    committing a newer v2 of the app - still in the making though

---
+++ app/src/main/java/com/example/android/cinematik/RecyclerViewItemDecorator.java
@@ -0,0 +1,30 @@
+package com.example.android.cinematik;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.annotation.DimenRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Sabina on 4/19/2018.
+ */
+
+public class RecyclerViewItemDecorator extends RecyclerView.ItemDecoration {
+
+    private int itemOffset;
+
+    public RecyclerViewItemDecorator(int itemOffset) {
+        this.itemOffset = itemOffset;
+    }
+
+    public RecyclerViewItemDecorator(Context context, @DimenRes int itemOffsetId) {
+        this(context.getResources().getDimensionPixelOffset(itemOffsetId));
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.set(itemOffset, itemOffset, itemOffset, itemOffset);
+    }
+}
